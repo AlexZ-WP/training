cb91753febd1b5ef821600efa4425c5fbc796f24
AlexZ-WP <alexz10@mail.ru>
2024-11-02 12:29:08 +0300

    Add files via upload

---
+++ training.py
@@ -0,0 +1,93 @@
+def square(x):
+    s = x ** 2
+    a = 6
+    if a>=s:
+        print(a)
+    else:
+        return s
+#print(a)
+print(square(4))
+
+
+a = 5
+b = square(2)
+print(b)
+print(a)
+
+# sum([5,6,10,18,27])
+# print(sum([5,6,10,18,27]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
